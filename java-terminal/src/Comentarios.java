bd10d22a4c5dfcd88834dbe7e0eee901e15d8527
rodrigo <24rodrigocurso@gmail.com>
2024-08-19 09:15:27 -0300

    Comentarios

---
+++ java-terminal/src/Comentarios.java
@@ -0,0 +1,12 @@
+public class Comentarios{
+    public static void main(String[] args) throws Exception {
+        //System.out.println("Hello, World!");
+    /*comentario em bloco
+     
+     */
+
+     /**Documentação
+      **/
+    
+    }
+}

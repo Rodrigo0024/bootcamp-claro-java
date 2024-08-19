3ae96876fc3471b99e3ca3bf87bd25d9b2b7b5d9
rodrigo <24rodrigocurso@gmail.com>
2024-08-19 12:45:39 -0300

    Passando argumentos artaves do terminal

---
+++ java-terminal/terminal-argumentos-1/src/MinhaClasse.java
@@ -0,0 +1,9 @@
+public class MinhaClasse {
+    public static void main(String[] args)  {
+        System.out.println("_______________________________");
+
+        String nome = "claudio";
+        System.out.println(nome+" Fui executado pelo terminal!");
+        System.out.println("_______________________________");
+    }
+}
